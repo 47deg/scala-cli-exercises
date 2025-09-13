f0365f1fee2b12760bd5a4ed1e9573da07dae3da
gummadi <gummadi@interobject.com>
2025-09-12 23:50:38 -0400

    custom toolkit: use project.scala to create custom toolkit. Upgrade dependencies
    scala-cli doesn't support centralizing dependencies with project.scala anymore

---
+++ custom-toolkit/xebia-se-toolkit.scala
@@ -0,0 +1,25 @@
+// Main
+//> using scala "3" "3.3.6"
+//> using platforms "scala-js"
+//> using toolkit "typelevel:0.1.29"
+
+//> using dependency "com.raquo::airstream::17.2.1"
+//> using dependency "com.raquo::laminar::17.2.1"
+//> using dependency "io.circe::circe-core:0.14.14"
+//> using dependency "io.circe::circe-generic:0.14.14"
+//> using dependency "io.circe::circe-parser:0.14.14"
+//> using dependency "org.scala-js::scalajs-dom::2.8.1"
+//> using dependency "org.scalatest::scalatest::3.2.19"
+//> using dependency "org.tpolecat::doobie-core:1.0.0-RC5"
+//> using dependency "org.tpolecat::doobie-postgres:1.0.0-RC4"
+//> using dependency "org.typelevel::cats-core:2.13.0"
+//> using dependency "org.typelevel::cats-kernel:2.13.0"
+//> using dependency "org.typelevel::cats-effect:3.6.3"
+
+// Test
+//> using test.dependency "org.scalameta::munit-scalacheck:1.2.0"
+//> using test.dependency "org.scalameta::munit:1.1.2"
+//> using test.dependency "org.scalatest::scalatest::3.2.19"
+//> using test.dependency "org.typelevel::munit-cats-effect:2.1.0"
+//> using test.dependency "org.typelevel::scalacheck-effect-munit:2.0.0-M2"
+
