d3b6907d6671ecb1e7599fea4e4f585c63085e54
gummadi <gummadi@interobject.com>
2025-09-15 04:21:14 -0400

    custom-toolkit, add publish-conf.scala

---
+++ custom-toolkit/publish-conf.scala
@@ -0,0 +1,7 @@
+//> using publish.organization com.xebia.SE
+//> using publish.version 0.1.1-SNAPSHOT
+//> using publish.url https://github.com/47deg/scala-cli-exercises
+//> using publish.license Apache-2.0
+//> using publish.repository central
+//> using publish.developer "xf|Xebia Functional|https://github.com/47deg"
+//> using repository sonatype:public
